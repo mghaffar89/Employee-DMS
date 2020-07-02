f01104858480601be688a6202879fa1836c316e5
Mina G <mghaffar89@gmail.com>
2020-07-01 23:52:40 -0500

    created first prompt to start app

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//REFERENCES FROM ACTIVITY 12.5, 12.10, 12.12, 12.13, & 12.14
+
 var mysql = require("mysql2");
 var inquirer = require("inquirer");
 
@@ -15,3 +17,39 @@ var connection = mysql.createConnection({
   password: "Mina123$",
   database: "employee_DB",
 });
+
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+function start() {
+  inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: "Would you like to do?",
+    choices: [
+      "View all Employees",
+      "View all Employees by Department",
+      "View all Employees by Manager",
+      "Add Employee",
+      "Add Department",
+      "Add Role",
+      "Remove Employee",
+      "Update Employee role",
+      "Exit",
+    ],
+  });
+
+  /*.then(function (answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid === "POST") {
+        postAuction();
+      } else if (answer.postOrBid === "BID") {
+        bidAuction();
+      } else {
+        connection.end();
+      }
+    });*/
+}
