826c08280f55d57cbcc92d0504dbc1d642c372c3
Mina G <mghaffar89@gmail.com>
2020-07-09 22:46:23 -0500

    added seeds file and fixed the schema

--- schema.sql
+++ schema.sql
@@ -12,17 +12,23 @@ PRIMARY KEY (id)
 CREATE TABLE role (
 id INT NOT NULL AUTO_INCREMENT,  
 title VARCHAR(30) NOT NULL,
-salary DEC(6,6) NOT NULL, 
-department_id INT(3) NOT NULL, 
-PRIMARY KEY (id)    
+salary DECIMAL UNSIGNED NOT NULL, 
+department_id INT(3) NOT NULL,
+INDEX dep_ind (department_id), 
+PRIMARY KEY (id), 
+CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE   
 );
 
 CREATE TABLE employee (
 id INT NOT NULL AUTO_INCREMENT, 
 first_name VARCHAR(30) NOT NULL, 
 last_name VARCHAR(30) NOT NULL, 
-rold_id INT(3) NOT NULL, 
-manager_id INT(3) NULL, 
-PRIMARY KEY (id)
+role_id INT NOT NULL,  
+PRIMARY KEY (id), 
+INDEX role_ind (role_id), 
+CONSTRAINT fk_role FOREGIN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE, 
+manager_id INT NULL,
+INDEX man_ind (manager_id),
+CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
---
+++ seed.sql
@@ -0,0 +1,40 @@
+use employee_DB;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Accounting'),
+    ('Administration'),
+    ('Warehouse'),
+    ('Management'), 
+    ('Customer Service'), 
+    ('Human Resources');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Account Manager', 80000, 2),
+    ('Accountant', 75000, 2),
+    ('Office Manager', 50000, 3),
+    ('Secretary', 45000, 3), 
+    ('Warehouse Worker', 45000, 4), 
+    ('Warehouse Manager', 65000, 4), 
+    ('Branch Manager', 120000, 5), 
+    ('Customer Service Rep', 50000, 6);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Jim', 'Halpert', 1, NULL),
+    ('Dwight', 'Schrute', 2, NULL),
+    ('Oscar', 'Martinez', 3, NULL),
+    ('Kevin', 'Malone', 4, NULL),
+    ('Pam', 'Beasley', 5, NULL),
+    ('Erin', 'Hannon', 6, NULL),
+    ('Val', 'Johnson', 7, NULL),
+    ('Daryl', 'Philbin', 8, NULL),
+    ('Michael', 'Scott', 9, 1),
+    ('Kelly', 'Kapoor', 10, NULL); 
